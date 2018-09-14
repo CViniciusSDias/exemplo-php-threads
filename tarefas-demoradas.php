3e4b4729850dcda29c53e07cf95e881867aad3e3
Vinicius Dias <carlosv775@gmail.com>
2018-09-14 13:35:59 +0000

    echos da main thread adicionados

--- tarefas-demoradas.php
+++ tarefas-demoradas.php
@@ -3,21 +3,25 @@
 $teste1 = new class extends \Thread {
     public function run()
     {
-        echo "Tarefa 1" . PHP_EOL;
+        echo 'Tarefa 1' . PHP_EOL;
         sleep(20);
-        echo "Fim da tarefa 1" . PHP_EOL;
+        echo 'Fim da tarefa 1' . PHP_EOL;
     }
 };
 
 $teste2 = new class extends \Thread {
     public function run()
     {
-        echo "Tarefa 2" . PHP_EOL;
+        echo 'Tarefa 2' . PHP_EOL;
         sleep(10);
-        echo "Fim da tarefa 2" . PHP_EOL;
+        echo 'Fim da tarefa 2' . PHP_EOL;
     }
 };
 
 echo PHP_EOL;
 $teste1->start();
 $teste2->start();
+
+echo 'Tarefa principal' . PHP_EOL;
+sleep(4);
+echo 'Fim da tarefa principal' . PHP_EOL;
